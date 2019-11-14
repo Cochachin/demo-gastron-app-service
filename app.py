e6ef87b4bd577782f66462664a962ec4a61ae9e5
Florian <efnj1235@gmail.com>
2019-11-13 22:34:49 -0500

    update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_restplus import Api
 blueprint = Blueprint('api', __name__, url_prefix='/api')
 api = Api(blueprint, doc='/docs')
 
-from app.controller.AuthController import auth as auth
+from src.controller.AuthController import auth as auth
 
 api.add_namespace(auth)
 
@@ -12,18 +12,3 @@ app = Flask(__name__)
 app.register_blueprint(blueprint)
 
 app.run(debug=True)
-
-#blueprint = Blueprint('api', __name__, url_prefix='/api/')
-#
-#api = Api(blueprint)
-
-#from app.controller.HelloController import HelloController
-
-
-#@ns.route('/hello')
-#class HelloWorld(Resource):
-#    def get(self):
-#        return {'hello': 'world'}
-#
-#if __name__ == '__main__':
-#    app.run(debug=True)
